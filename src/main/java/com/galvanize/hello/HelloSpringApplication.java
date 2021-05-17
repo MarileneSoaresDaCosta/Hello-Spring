ca52989ca38e28d84ff5fcafe5522ab4a423be8b
Rob Wing <rob.wing@galvanize.com>
2021-05-17 14:17:22 -0700

    Add controller & Dockerfile

--- src/main/java/com/galvanize/hello/HelloSpringApplication.java
+++ src/main/java/com/galvanize/hello/HelloSpringApplication.java
@@ -2,12 +2,20 @@ package com.galvanize.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String sayHello(@RequestParam(defaultValue = "World") String name){
+		return String.format("Hello %s", name);
+	}
 }
